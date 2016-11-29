782942506236df116972fdcd6706c1f6d414a208
Joseph Weissman <joe@bignerdranch.com>
2016-11-29 18:04:01 -0500

    initial draft of logic programming framework for ruby!
    with monotone laws

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,1 +1,373 @@
 require 'logic/version'
+
+module Logic
+  class Term
+    extend Forwardable
+    def_delegators :to_expression, :negate, :conjoin, :disjoin, :implies
+  end
+
+  class Constant < Term
+    attr_reader :name, :value
+
+    def initialize(name, value)
+      @name = name
+      @value = value
+    end
+
+    def to_expression
+      ConstantExpression.new(self.name, self.value)
+    end
+  end
+
+  Truth = Constant.new("T", true)
+  Falsity = Constant.new("F", false)
+
+  class Variable < Term
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def to_expression
+      SimpleExpression.new(self)
+    end
+  end
+
+  class Expression
+    def name
+      raise "TODO name this kind of expression (#{self.class.name})"
+    end
+
+    def describe
+      name
+    end
+
+    def reduce(env={})
+      raise "TODO implement #reduce for this kind of expression (#{self.class.name})"
+    end
+
+    # def to_s
+    #   name
+    # end
+    #
+    # def inspect
+    #   describe
+    # end
+
+    # expressions are considered 'equal' if satisfiers are equivalent
+    # note this has weird effect that all tautologically-false statements are equivalent
+    def ==(other)
+      satisfiers == other.satisfiers
+    end
+
+    def negate
+      NegatedExpression.new(self)
+    end
+
+    def conjoin(other)
+      ConjoinedExpression.new(self, expressionize(other))
+    end
+
+    def disjoin(other)
+      DisjoinedExpression.new(self, expressionize(other))
+    end
+
+    def implies(other)
+      ConditionalExpression.new(self, expressionize(other))
+    end
+
+    def bind(env)
+      BoundExpression.new(self, env)
+    end
+
+    def free_variables
+      raise "TODO identify free vars in this kind of expression (#{self.class.name})"
+    end
+
+    def satisfiable?
+      if free_variables.any?
+        # pick off free variables recursively?
+        var_to_bind = free_variables.first
+        var_key = var_to_bind.name.to_sym
+
+        # try true path first...
+        bound_true = bind(var_key => true)
+        return true if bound_true.satisfiable?
+
+        bound_false = bind(var_key => false)
+        return true if bound_false.satisfiable?
+
+        false
+      else
+        # no free vars, just eval
+        evaluate
+      end
+    end
+
+    def context
+      {}
+    end
+
+    def satisfiers
+      sat_matches = []
+
+      if free_variables.any?
+        var_to_bind = free_variables.first
+        var_key = var_to_bind.name.to_sym
+
+        bound_true = bind(var_key => true)
+        bound_true.satisfiers.each do |true_sat|
+          sat_matches.push({var_key => true}.merge(true_sat))
+        end
+
+        bound_false = bind(var_key => false)
+        bound_false.satisfiers.each do |false_sat|
+          sat_matches.push({var_key => false}.merge(false_sat))
+        end
+
+        sat_matches
+      else
+        if satisfiable?
+          [context]
+        else
+          []
+        end
+      end
+    end
+
+    protected
+    def parenthesize(str)
+      if (logic_symbols - ['~']).any? { |sym| str.include?(sym) } || (logic_words - ['not']).any? { |sym| str.include?(sym) }
+        "(#{str})"
+      else
+        str
+      end
+    end
+
+    private
+    def logic_symbols
+      %w[ ^ v ~ -> ]
+    end
+
+    def logic_words
+      %w[ and or not then ]
+    end
+
+    def expressionize(thing)
+      return thing if thing.is_a?(Expression)
+      if thing.is_a?(Variable)
+        thing.to_expression
+      else
+        raise "Unknown term kind #{thing.class.name} (unable to coerce into expression)"
+      end
+    end
+  end
+
+  class ConstantExpression < Expression
+    attr_reader :name, :value
+
+    def initialize(name, value)
+      @name = name
+      @value = value
+    end
+
+    def free_variables
+      []
+    end
+
+    def evaluate(*)
+      value
+    end
+  end
+
+  class SimpleExpression < Expression
+    def initialize(term)
+      @term = term
+    end
+
+    def name
+      term.name
+    end
+
+    def describe
+      name
+    end
+
+    def free_variables
+      [term]
+    end
+
+    def evaluate(env={})
+      raise "Env (#{env}) does not contain term #{term.name}" unless env.include?(name.to_sym)
+      env[name.to_sym]
+    end
+
+    def reduce(env={})
+      if env.has_key?(name.to_sym)
+        lift_bool evaluate(env)
+      else
+        self
+      end
+    end
+
+    def lift_bool(true_or_false)
+      if true_or_false
+        Truth.to_expression
+      else
+        Falsity.to_expression
+      end
+    end
+
+    private
+    attr_reader :term
+  end
+
+  class NegatedExpression < Expression
+    def initialize(expression)
+      @expression = expression
+    end
+
+    def name
+      "~#{expression.name}"
+    end
+
+    def describe
+      "not #{expression.describe}"
+    end
+
+    def free_variables
+      expression.free_variables
+    end
+
+    def evaluate(env={})
+      !expression.evaluate(env)
+    end
+
+    private
+    attr_reader :expression
+  end
+
+  class BoundExpression < Expression
+    def initialize(expression, env={})
+      @expression = expression
+      @context = env
+    end
+
+    def context
+      @context
+    end
+
+    def reduce
+      @expression.reduce(@context)
+    end
+
+    # ignore bound vars here...
+    def free_variables
+      @expression.free_variables.reject do |var|
+        @context.keys.include?(var.name.to_sym)
+      end.sort_by(&:name)
+    end
+
+    def evaluate(env={})
+      @expression.evaluate(@context.merge(env))
+    end
+  end
+
+  class BinaryExpression < Expression
+    attr_reader :left, :right
+
+    def initialize(left, right)
+      raise unless left.is_a?(Expression) && right.is_a?(Expression)
+      @left = left
+      @right = right
+    end
+
+    def name
+      "#{parenthesize @left.name}#{operator_glyph}#{parenthesize @right.name}"
+    end
+
+    def describe
+      "#{parenthesize @left.describe} #{operator_description} #{parenthesize @right.describe}"
+    end
+
+    def free_variables
+      (@left.free_variables + @right.free_variables).uniq.sort_by(&:name)
+    end
+
+    def operator_glyph
+      raise "override #operator_name in #{self.class.name}"
+    end
+
+    def operator_description
+      raise "override #operator_description in #{self.class.name}"
+    end
+  end
+
+  class ConjoinedExpression < BinaryExpression
+    def evaluate(env={})
+      @left.evaluate(env) && @right.evaluate(env)
+    end
+
+    def operator_glyph
+      '^'
+    end
+
+    def operator_description
+      'and'
+    end
+
+    def reduce(env={})
+      if (@right.reduce(env)) == Truth.to_expression
+        @left.reduce(env)
+      elsif (@right.reduce(env)) == Falsity.to_expression
+        Falsity.to_expression
+      elsif (@right.is_a?(DisjoinedExpression) && @right.left == @left)
+        @left
+      else
+        self
+        # Falsity.to_expression
+      end
+    end
+  end
+
+  class DisjoinedExpression < BinaryExpression
+    def evaluate(env={})
+      @left.evaluate(env) || @right.evaluate(env)
+    end
+
+    def operator_glyph
+      'v'
+    end
+
+    def operator_description
+      'or'
+    end
+
+    def reduce(env={})
+      if @right.reduce(env) == Falsity.to_expression
+        @left.reduce(env)
+      elsif @right.reduce(env) == Truth.to_expression
+        Truth.to_expression
+      elsif (@right.is_a?(ConjoinedExpression) && @right.left == @left)
+        @left
+      else
+        self
+      end
+    end
+  end
+
+  class ConditionalExpression < BinaryExpression
+    def evaluate(env={})
+      @left.negate.disjoin(@right).evaluate(env)
+    end
+
+    def operator_glyph
+      '->'
+    end
+
+    def operator_description
+      'then'
+    end
+  end
+end
--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -1,8 +1,204 @@
 require 'spec_helper'
+require 'pry'
 require 'logic'
 
 describe Logic do
-  it "should have a VERSION constant" do
-    expect(subject.const_get('VERSION')).to_not be_empty
+  # cf https://en.wikipedia.org/wiki/Boolean_algebra#Laws
+  let(:x) { Variable.new('x') }
+  let(:y) { Variable.new('y') }
+  let(:z) { Variable.new('z') }
+
+  context 'according to the monotone laws:' do
+    describe 'conjunction' do
+      it 'has an identity (true)' do
+        expect(x.conjoin(y).bind(y: true).reduce).to eq(x.to_expression)
+      end
+
+      it 'has an annihilator (false)' do
+        expect(x.conjoin(y).bind(y: false).reduce).to eq(Falsity.to_expression)
+      end
+
+      it 'is associative' do
+        expect(x.conjoin(y.conjoin(z))).to eq((x.conjoin(y)).conjoin(z))
+      end
+
+      it 'is commutative' do
+        expect(x.conjoin(y)).to eq(y.conjoin(x))
+      end
+
+      it 'is idempotent' do
+        expect(x.conjoin(x).reduce).to eq(x.to_expression)
+      end
+
+      it 'is distributive over disjunction' do
+        expect(x.conjoin(y.disjoin(z))).to eq((x.conjoin(y)).disjoin(x.conjoin(z)))
+      end
+
+      it 'absorbs disjunctions' do
+        expect(x.conjoin(x.disjoin(y)).reduce).to eq(x.to_expression)
+      end
+    end
+
+    describe 'disjunction' do
+      it 'has an identity (false)' do
+        expect(x.disjoin(y).bind(y: false).reduce).to eq(x.to_expression)
+      end
+
+      it 'has an annihilator (true)' do
+        expect(x.disjoin(y).bind(y: true).reduce).to eq(Truth.to_expression)
+      end
+
+      it 'is associative' do
+        expect(x.disjoin(y.disjoin(z))).to eq((x.disjoin(y)).disjoin(z))
+      end
+
+      it 'is idempotent' do
+        expect(x.disjoin(x).reduce).to eq(x.to_expression)
+      end
+
+      it 'is commutative' do
+        expect(x.disjoin(y)).to eq(y.disjoin(x))
+      end
+
+      it 'is distributive over conjunction' do
+        expect(x.disjoin(y.conjoin(z))).to eq((x.disjoin(y)).conjoin(x.disjoin(z)))
+      end
+
+      it 'absorbs conjunctions' do
+        expect(x.disjoin(x.conjoin(y)).reduce).to eq(x.to_expression)
+      end
+    end
+  end
+end
+
+describe Term do
+  describe Constant do
+    it 'always has a standard truth-functional value' do
+      expect(Truth.value).to be_truthy
+      expect(Falsity.value).to be_falsy
+    end
+
+    let(:not_true) { Truth.negate }
+
+    it 'can be negated' do
+      expect(not_true).to be_an(Expression)
+      expect(not_true.name).to eq('~T')
+      expect(not_true.describe).to eq('not T')
+      expect(not_true.evaluate).to be_falsy
+    end
+  end
+
+  describe Variable do
+    let(:a) { Variable.new('a') }
+    let(:not_a) { a.negate }
+    let(:b) { Variable.new('b') }
+    let(:a_and_b) { a.conjoin(b) }
+    let(:b_and_a) { b.conjoin(a) }
+    let(:a_or_b) { a.disjoin(b) }
+    let(:b_or_a) { b.disjoin(a) }
+    let(:a_then_b) { a.implies(b) }
+    let(:b_then_a) { b.implies(a) }
+
+    it 'has an placeholder truth-functional value' do
+      expect(a.name).to eq('a')
+      expect(a.to_expression.evaluate(a: true)).to be_truthy
+      expect(a.to_expression.evaluate(a: false)).to be_falsy
+    end
+
+    it 'can be negated' do
+      expect(not_a).to be_an(Expression)
+      expect(not_a.name).to eq('~a')
+      expect(not_a.evaluate(a: true)).to be_falsy
+      expect(not_a.evaluate(a: false)).to be_truthy
+    end
+
+    it 'can be conjoined' do
+      expect(a_and_b.name).to eq('a^b')
+      expect(b_and_a.name).to eq('b^a')
+
+      expect(a_and_b.evaluate(a: true,  b: true)).to be_truthy
+      expect(a_and_b.evaluate(a: true,  b: false)).to be_falsy
+      expect(a_and_b.evaluate(a: false, b: true)).to be_falsy
+      expect(a_and_b.evaluate(a: false, b: false)).to be_falsy
+
+      expect(b_and_a.evaluate(a: true,  b: true)).to be_truthy
+      expect(b_and_a.evaluate(a: true,  b: false)).to be_falsy
+      expect(b_and_a.evaluate(a: false, b: true)).to be_falsy
+      expect(b_and_a.evaluate(a: false, b: false)).to be_falsy
+    end
+
+    it 'can be disjoined' do
+      expect(a_or_b.name).to eq('avb')
+      expect(b_or_a.name).to eq('bva')
+
+      expect(a_or_b.evaluate(a: true,  b: true)).to be_truthy
+      expect(a_or_b.evaluate(a: true,  b: false)).to be_truthy
+      expect(a_or_b.evaluate(a: false, b: true)).to be_truthy
+      expect(a_or_b.evaluate(a: false, b: false)).to be_falsy
+
+      expect(b_or_a.evaluate(a: true,  b: true)).to be_truthy
+      expect(b_or_a.evaluate(a: true,  b: false)).to be_truthy
+      expect(b_or_a.evaluate(a: false, b: true)).to be_truthy
+      expect(b_or_a.evaluate(a: false, b: false)).to be_falsy
+    end
+
+    it 'can be implicated' do
+      expect(a_then_b.name).to eq('a->b')
+      expect(b_then_a.name).to eq('b->a')
+
+      expect(a_then_b.evaluate(a: true,  b: true)).to be_truthy
+      expect(a_then_b.evaluate(a: true,  b: false)).to be_falsy
+      expect(a_then_b.evaluate(a: false, b: true)).to be_truthy
+      expect(a_then_b.evaluate(a: false, b: false)).to be_truthy
+
+      expect(b_then_a.evaluate(a: true,  b: true)).to be_truthy
+      expect(b_then_a.evaluate(a: true,  b: false)).to be_truthy
+      expect(b_then_a.evaluate(a: false, b: true)).to be_falsy
+      expect(b_then_a.evaluate(a: false, b: false)).to be_truthy
+    end
+  end
+
+  describe Expression do
+    let(:a_prime) { Variable.new('a') }
+    let(:a) { SimpleExpression.new(a_prime) }
+
+    let(:b_prime) { Variable.new('b') }
+    let(:b) { SimpleExpression.new(b_prime) }
+
+    it 'can be narrated' do
+      expect(a.describe).to eq("a")
+      expect(a.negate.describe).to eq("not a")
+      expect(a.conjoin(a).describe).to eq("a and a")
+      expect(a.disjoin(a).describe).to eq("a or a")
+      expect(a.implies(a).describe).to eq("a then a")
+
+      expect(b.describe).to eq("b")
+      expect(b.implies(a.negate.conjoin(b)).describe).to eq("b then (not a and b)")
+    end
+
+    it 'can identify free variables' do
+      expect(a.free_variables).to eq([a_prime])
+      expect(b.free_variables).to eq([b_prime])
+
+      expect(a.implies(b).free_variables).to eq([a_prime, b_prime])
+      expect(a.negate.implies(b.conjoin(a.disjoin(b))).free_variables).to eq([a_prime, b_prime])
+    end
+
+    it 'can bind variables' do
+      expect(a.bind(a: false).evaluate).to eq(false)
+      expect(b.bind(b: true).evaluate).to eq(true)
+    end
+
+    it 'can be satisfied' do
+      expect(a.satisfiable?).to eq(true)
+      expect(a.conjoin(a.negate).satisfiable?).to eq(false)
+    end
+
+    it 'can identify satisfying cases' do
+      expect(a.satisfiers).to eq([{a: true}])
+      expect(a.conjoin(a.negate).satisfiers).to eq([])
+      expect(a.implies(b).negate.satisfiers).to eq([{a: true, b: false}])
+      expect(a.disjoin(b).satisfiers).to eq([{a: true, b: true}, {a: true, b: false}, {a: false, b: true}])
+    end
   end
 end
